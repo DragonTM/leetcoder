df4a5dc4cc14884538772dc10fb5ae09715f0504
ivankushnir <ivan.kushnir.work@gmail.com>
2024-07-24 23:03:29 +0300

    238 slow and fast solutions

--- LeetCoder/238_ProductOfArray/ProductOfArray.cs
+++ LeetCoder/238_ProductOfArray/ProductOfArray.cs
@@ -4,6 +4,46 @@ public static class ProductOfArray
 {
     public static int[] ProductExceptSelf(int[] nums)
     {
-        return null;
+        var prefix = new int[nums.Length];
+        prefix[0] = 1;
+        var zeroCount = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            prefix[i] = nums[i - 1] * prefix[i -1];
+
+            if (nums[i] == 0)
+            {
+                zeroCount++;
+            }
+        }
+
+        if (zeroCount > 1)
+        {
+            return new int[nums.Length];
+        }
+        
+        var sufix = new int[nums.Length];
+        sufix[nums.Length - 1] = 1;
+        for (var i = nums.Length - 2; i >= 0; i--)
+        {
+            sufix[i] = nums[i+1] * sufix[i+1];
+        }
+        
+        var result = new int[nums.Length];
+        for (var i = 0; i < result.Length; i++)
+        {
+            result[i] = prefix[i] * sufix[i];
+        }
+
+        return result;
     }
+
+    // Simple but slow
+    // public static int[] ProductExceptSelf(int[] nums)
+    // {
+    //     return nums
+    //         .Select((_, index) =>
+    //             nums.Where((_, i) => i != index).Aggregate(1, (a, c) => a * c))
+    //         .ToArray();
+    // }
 }
--- LeetCoder/238_ProductOfArray/ProductOfArrayTests.cs
+++ LeetCoder/238_ProductOfArray/ProductOfArrayTests.cs
@@ -4,7 +4,7 @@ namespace LeetCoder._238_ProductOfArray;
 public class ProductOfArrayTests
 {
     [TestCase(new int[]{1,2,3,4}, new int[]{24,12,8,6})]
-    [TestCase(new int[]{-1,1,0,-3,}, new int[]{0,0,9,0,0})]
+    [TestCase(new int[]{-1,1,0,-3,3}, new int[]{0,0,9,0,0})]
     public void ProductExceptSelfTest(int[] nums, int[] result)
     {
         var product = ProductOfArray.ProductExceptSelf(nums);
